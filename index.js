414b056e63563c5c1352598e8b32d78c6c6efe58
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-08-09 10:49:03 -0400

    removed line breaks in index.js

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 let button = document.querySelector('#middle-button')
-
 button.addEventListener('click', sayHi) 
-
 function sayHi() { alert('Hi') }
 
 
 let div = document.querySelector('.div-container')
-
 div.addEventListener('dblclick', makeDivOrange)
-
 function makeDivOrange() { div.style.backgroundColor = "orange" }
