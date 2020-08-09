23ddb28e17ae5bb085381089debbd88bd4fe1662
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-08-09 00:14:57 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let button = document.querySelector('#middle-button')
+
+button.addEventListener('click', sayHi) 
+
+function sayHi() { alert('Hi') }
+
+
+let div = document.querySelector('.div-container')
+
+div.addEventListener('dblclick', makeDivOrange)
+
+function makeDivOrange() { div.style.backgroundColor = "orange" }
